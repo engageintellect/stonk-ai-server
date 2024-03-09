1ccfe0de67557d67eb7434c710f0838d8c99137c
engageintellect <jc9361@gmail.com>
2024-03-09 00:34:27 -0800

    init

--- main.py
+++ main.py
@@ -168,4 +168,6 @@ def get_stock_prediction(ticker: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, ssl_keyfile="/etc/letsencrypt/live/engage-dev.com/privkey.pem", ssl_certfile="/etc/letsencrypt/live/engage-dev.com/fullchain.pem")
+
