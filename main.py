40b79b65403a1204459fb681f0cf907db7c28771
engageintellect <jc9361@gmail.com>
2024-03-12 17:49:03 -0700

    init

--- main.py
+++ main.py
@@ -180,3 +180,4 @@ def get_stock_prediction(ticker: str):
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000, ssl_keyfile="/etc/letsencrypt/live/engage-dev.com/privkey.pem", ssl_certfile="/etc/letsencrypt/live/engage-dev.com/fullchain.pem")
